653fa3c42870c052023b7d6dd3b63d84ad8de8ca
shikhar0208 <shikharrastogi.0208@gmail.com>
2022-03-10 17:29:18 +0530

    change baseurl

--- src/redux/api.js
+++ src/redux/api.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 
 // for live server
 const API = axios.create({
-  baseURL: 'https://fanstar-kylo.herokuapp.com/',
+  baseURL: 'https://fanstar-backend-uiwtg.ondigitalocean.app/',
 });
 
 // https://fanstar-kylo.herokuapp.com/
